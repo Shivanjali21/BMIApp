7dbc688d824e6f5b5fc2ad5237890ed482657810
Shivi <shivanjaligupta10@gmail.com>
2025-01-13 19:35:33 +0530

    1. Added round button. 2. Hardcode string format. 3. Added Image Assets. 4. Hide Keyboard.

--- app/src/main/java/com/practice/bmi/MainActivity.java
+++ app/src/main/java/com/practice/bmi/MainActivity.java
@@ -1,13 +1,16 @@
 package com.practice.bmi;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.LinearLayout;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.AppCompatEditText;
 import androidx.appcompat.widget.AppCompatTextView;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,27 +33,33 @@ public class MainActivity extends AppCompatActivity {
         llMain = findViewById(R.id.llMain);
 
         btnCalculateBMI.setOnClickListener( v-> {
-           int wt = Integer.parseInt(edtWeight.getText().toString());
-           int ft = Integer.parseInt(edtHeightFt.getText().toString());
-           int in = Integer.parseInt(edtHeightIn.getText().toString());
+           int wt = Integer.parseInt(Objects.requireNonNull(edtWeight.getText()).toString());
+           int ft = Integer.parseInt(Objects.requireNonNull(edtHeightFt.getText()).toString());
+           int in = Integer.parseInt(Objects.requireNonNull(edtHeightIn.getText()).toString());
 
            int totalIn = ft * 12 + in;
            double totalCM = totalIn * 2.53;
            double totalIM = totalCM / 100;
            double bmi = wt/(totalIM*totalIM);
            if(bmi > 25) {
-             tvResult.setText("You are overweight");
+             tvResult.setText(String.format("Your BMI is " + bmi + "\n" + getResources().getText(R.string.ow)));
              llMain.setBackgroundColor(getResources().getColor(R.color.color_ow));
              tvResult.setTextColor(getResources().getColor(R.color.black));
            }else if(bmi < 18){
-               tvResult.setText("You are underweight");
+               tvResult.setText(String.format("Your BMI is " + bmi + "\n" + getResources().getText(R.string.uw)));
                llMain.setBackgroundColor(getResources().getColor(R.color.color_uw));
                tvResult.setTextColor(getResources().getColor(R.color.white));
            }else {
-               tvResult.setText("You are healthy");
+               tvResult.setText(String.format("Your BMI is " + bmi + "\n" + getResources().getText(R.string.h)));
                llMain.setBackgroundColor(getResources().getColor(R.color.color_h));
                tvResult.setTextColor(getResources().getColor(R.color.white));
            }
+           closeKeyboard();
         });
+    }
+
+    private void closeKeyboard() {
+      InputMethodManager inputManager = (InputMethodManager)this.getSystemService(Context.INPUT_METHOD_SERVICE);
+      inputManager.hideSoftInputFromWindow(Objects.requireNonNull(this.getCurrentFocus()).getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
     }
 }
