9bc72184c0fc56e6c9001df67153e0166d9c4ce1
Shivi <shivanjaligupta10@gmail.com>
2025-03-13 07:50:22 +0530

    initial commit for simple app of playing music.

--- app/src/main/java/com/practice/bmi/MainActivity.java
+++ app/src/main/java/com/practice/bmi/MainActivity.java
@@ -1,20 +1,19 @@
 package com.practice.bmi;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import com.practice.bmi.databinding.ActivityMainBinding;
-import java.util.Objects;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding activityMainBinding;
-    private final int CAMERA_REQ_CODE = 1;
-    private final int GALLERY_REQ_CODE = 2;
+    MediaPlayer mp;
+    String localPath, onlinePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,30 +22,27 @@ public class MainActivity extends AppCompatActivity {
         View view = activityMainBinding.getRoot();
         setContentView(view);
 
-        activityMainBinding.btnOpenCamera.setOnClickListener(v -> {
-            Intent iCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            startActivityForResult(iCamera, CAMERA_REQ_CODE);
-        });
+        mp = new MediaPlayer();
+        mp.setAudioStreamType(AudioManager.STREAM_MUSIC);
 
-        activityMainBinding.btnOpenGallery.setOnClickListener(v-> {
-           Intent iGallery = new Intent(Intent.ACTION_PICK);
-           iGallery.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-           startActivityForResult(iGallery, GALLERY_REQ_CODE);
-        });
-    }
+        localPath = "android.resource://"+getPackageName()+"/raw/relaxing_piano";
+        onlinePath = "";
+        Uri pathParse = Uri.parse(localPath);
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if(resultCode == RESULT_OK){
-           if(requestCode == CAMERA_REQ_CODE){
-             assert data != null;
-             Bitmap imgBitmap = (Bitmap) Objects.requireNonNull(data.getExtras()).get("data");
-             activityMainBinding.ivCameraGallery.setImageBitmap(imgBitmap);
-           }else if(requestCode == GALLERY_REQ_CODE){
-             activityMainBinding.ivCameraGallery.setImageURI(data.getData());
-           }
+        try {
+            mp.setDataSource(this, pathParse);
+            mp.prepare();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+
+        activityMainBinding.btnPlay.setOnClickListener(v -> mp.start());
+
+        activityMainBinding.btnPause.setOnClickListener(v-> mp.pause());
+
+        activityMainBinding.btnStop.setOnClickListener(v -> {
+          mp.pause();
+          mp.seekTo(0);
+        });
     }
 }
