d00ed9246e1d949d76e8b7798c66cbb1b58e1c65
Shivi <shivanjaligupta10@gmail.com>
2025-03-13 06:30:25 +0530

    initial commit for simple app of setting image via camera or gallery.

--- app/src/main/java/com/practice/bmi/MainActivity.java
+++ app/src/main/java/com/practice/bmi/MainActivity.java
@@ -1,29 +1,20 @@
 package com.practice.bmi;
 
-import android.app.Activity;
-import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.os.Handler;
-import android.text.Editable;
-import android.text.TextUtils;
-import android.text.TextWatcher;
-import android.util.Log;
+import android.provider.MediaStore;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
-import androidx.core.content.res.ResourcesCompat;
 import com.practice.bmi.databinding.ActivityMainBinding;
-import java.util.ArrayList;
 import java.util.Objects;
-import www.sanju.motiontoast.MotionToast;
-import www.sanju.motiontoast.MotionToastStyle;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding activityMainBinding;
-    private SharedPrefTheme sp;
-    private boolean isDarkMode;
+    private final int CAMERA_REQ_CODE = 1;
+    private final int GALLERY_REQ_CODE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,94 +23,30 @@ public class MainActivity extends AppCompatActivity {
         View view = activityMainBinding.getRoot();
         setContentView(view);
 
-        ExpenseDatabaseHelper expenseDB = ExpenseDatabaseHelper.getDB(this);
-        sp = new SharedPrefTheme(this);
-        isDarkMode = sp.loadNightModeState();
+        activityMainBinding.btnOpenCamera.setOnClickListener(v -> {
+            Intent iCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(iCamera, CAMERA_REQ_CODE);
+        });
 
-        fieldValidationButtonActive();
-
-        // Set the initial state of the switch based on the saved theme
-        activityMainBinding.themeSwitch.setChecked(isDarkMode);
-        updateTheme(isDarkMode);
-
-        activityMainBinding.themeSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> updateTheme(isChecked));
-
-        activityMainBinding.etExpenseName.addTextChangedListener(createTextWatcher());
-        activityMainBinding.etExpenseAmount.addTextChangedListener(createTextWatcher());
-
-        activityMainBinding.btnAddExpense.setOnClickListener(v -> {
-            String userExpense = Objects.requireNonNull(activityMainBinding.etExpenseName.getText()).toString();
-            String userAmount = Objects.requireNonNull(activityMainBinding.etExpenseAmount.getText()).toString();
-
-            if (TextUtils.isEmpty(userExpense)) {
-                MotionToast.Companion.createColorToast(this, "", "Please Enter Expense Name", MotionToastStyle.ERROR,
-                        MotionToast.GRAVITY_BOTTOM, MotionToast.LONG_DURATION, ResourcesCompat.getFont(this, R.font.atkinson_medium));
-            } else if (TextUtils.isEmpty(userAmount)) {
-                MotionToast.Companion.createColorToast(this, "", "Please Enter Expense Amount", MotionToastStyle.ERROR,
-                        MotionToast.GRAVITY_BOTTOM, MotionToast.LONG_DURATION, ResourcesCompat.getFont(this, R.font.atkinson_medium));
-            } else {
-                expenseDB.expenseDAO().addTx(new ExpenseModel(userExpense, userAmount));
-                new Handler().postDelayed(() -> {
-                    activityMainBinding.etExpenseName.setText("");
-                    activityMainBinding.etExpenseAmount.setText("");
-                    MotionToast.Companion.createColorToast(this, "Congratulations!", "Item Added Successfully!", MotionToastStyle.SUCCESS,
-                            MotionToast.GRAVITY_BOTTOM, MotionToast.LONG_DURATION, ResourcesCompat.getFont(this, R.font.atkinson_medium));
-
-                    hideKeyboardFrom(this, activityMainBinding.llMain);
-                }, 2000);
-
-                ArrayList<ExpenseModel> arrayExpense = (ArrayList<ExpenseModel>) expenseDB.expenseDAO().getAllExpenses();
-                for (int i = 0; i < arrayExpense.size(); i++) {
-                    Log.d("MainActivity", "Expense Name: " + arrayExpense.get(i).getExpenseName() + "Expense Amount: " + arrayExpense.get(i).getExpenseAmount());
-                }
-            }
+        activityMainBinding.btnOpenGallery.setOnClickListener(v-> {
+           Intent iGallery = new Intent(Intent.ACTION_PICK);
+           iGallery.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+           startActivityForResult(iGallery, GALLERY_REQ_CODE);
         });
     }
 
-    // Method to update the theme
-    private void updateTheme(boolean isChecked) {
-        if (isChecked) {
-            activityMainBinding.themeSwitch.setChecked(true);
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-            sp.setNightModeState(true);
-        } else {
-            activityMainBinding.themeSwitch.setChecked(false);
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-            sp.setNightModeState(false);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK){
+           if(requestCode == CAMERA_REQ_CODE){
+             assert data != null;
+             Bitmap imgBitmap = (Bitmap) Objects.requireNonNull(data.getExtras()).get("data");
+             activityMainBinding.ivCameraGallery.setImageBitmap(imgBitmap);
+           }else if(requestCode == GALLERY_REQ_CODE){
+             activityMainBinding.ivCameraGallery.setImageURI(data.getData());
+           }
         }
-        isDarkMode = isChecked; // Update isDarkMode to reflect the switch state
-    }
-
-    // Method to create a TextWatcher
-    private TextWatcher createTextWatcher() {
-        return new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                // Update button state in real-time as user types
-                fieldValidationButtonActive();
-            }
-            @Override
-            public void afterTextChanged(Editable s) {}
-        };
-    }
-
-    private void fieldValidationButtonActive() {
-        String userExpense = Objects.requireNonNull(activityMainBinding.etExpenseName.getText()).toString();
-        String userAmount = Objects.requireNonNull(activityMainBinding.etExpenseAmount.getText()).toString();
-        if (!TextUtils.isEmpty(userExpense) && !TextUtils.isEmpty(userAmount)) {
-            //activityMainBinding.btnAddExpense.setEnabled(true);
-            activityMainBinding.btnAddExpense.setBackgroundColor(getResources().getColor(R.color.color_green));
-        } else {
-            //activityMainBinding.btnAddExpense.setEnabled(false);
-            activityMainBinding.btnAddExpense.setBackgroundColor(getResources().getColor(R.color.color_grey));
-        }
-    }
-
-    /*To hide Keyboard */
-    public static void hideKeyboardFrom(Context context, View view) {
-        InputMethodManager imm = (InputMethodManager) context.getSystemService(Activity.INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
