a0acfb91c0dfc59e4a70db649d659fd2cdfdb611
Shivi <shivanjaligupta10@gmail.com>
2025-03-31 23:28:05 +0530

    initial commit for calculator in kotlin.

--- app/src/main/java/com/practice/bmi/MainActivity.kt
+++ app/src/main/java/com/practice/bmi/MainActivity.kt
@@ -1,29 +1,99 @@
-package com.practice.bmi;
+package com.practice.bmi
 
-import android.app.Activity;
-import android.content.Context;
-import android.os.Bundle;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import androidx.appcompat.app.AppCompatActivity;
-import com.practice.bmi.databinding.ActivityMainBinding;
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.practice.bmi.databinding.ActivityMainBinding
 
-public class MainActivity extends AppCompatActivity {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    private ActivityMainBinding activityMainBinding;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
-        View view = activityMainBinding.getRoot();
-        setContentView(view);
-
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
     }
 
-    public static void hideKeyboardFrom(Context context, View view) {
-        InputMethodManager imm = (InputMethodManager) context.getSystemService(Activity.INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.btnAdd.setOnClickListener(this)
+        binding.btnSub.setOnClickListener(this)
+        binding.btnMul.setOnClickListener(this)
+        binding.btnDiv.setOnClickListener(this)
+    }
+
+    fun hideKeyboardFrom(context: Context, view: View) {
+        val imm = context.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.btnAdd -> {
+                if (binding.etValueOne.text.toString() != "" && binding.etValueTwo.text.toString() != "") {
+                    val userInputOne = binding.etValueOne.text.toString().toInt()
+                    val userInputTwo = binding.etValueTwo.text.toString().toInt()
+                    val sum = userInputOne + userInputTwo
+                    Toast.makeText(this, "The sum is $sum", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Please fill all the required fields!", Toast.LENGTH_SHORT)
+                        .show()
+                }
+                binding.etValueOne.setText("")
+                binding.etValueTwo.setText("")
+            }
+
+            R.id.btnSub -> {
+                if (binding.etValueOne.text.toString() != "" && binding.etValueTwo.text.toString() != "") {
+                    val userInputOne = binding.etValueOne.text.toString().toInt()
+                    val userInputTwo = binding.etValueTwo.text.toString().toInt()
+                    val sub = if (userInputOne > userInputTwo) {
+                        userInputOne - userInputTwo
+                    } else {
+                        userInputTwo - userInputOne
+                    }
+                    Toast.makeText(this, "The difference is $sub", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Please fill all the required fields!", Toast.LENGTH_SHORT)
+                        .show()
+                }
+                binding.etValueOne.setText("")
+                binding.etValueTwo.setText("")
+            }
+
+            R.id.btnMul -> {
+                if (binding.etValueOne.text.toString() != "" && binding.etValueTwo.text.toString() != "") {
+                    val userInputOne = binding.etValueOne.text.toString().toInt()
+                    val userInputTwo = binding.etValueTwo.text.toString().toInt()
+                    val mul = userInputOne * userInputTwo
+                    Toast.makeText(this, "The product is $mul", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Please fill all the required fields!", Toast.LENGTH_SHORT)
+                        .show()
+                }
+                binding.etValueOne.setText("")
+                binding.etValueTwo.setText("")
+            }
+
+            R.id.btnDiv -> {
+                val div : Int
+                if (binding.etValueOne.text.toString() != "" && binding.etValueTwo.text.toString() != "") {
+                    val userInputOne = binding.etValueOne.text.toString().toInt()
+                    val userInputTwo = binding.etValueTwo.text.toString().toInt()
+                    if (userInputTwo == 0) {
+                        Toast.makeText(this, "A number can not de divided by 0 as it produce infinite result.", Toast.LENGTH_SHORT).show()
+                    } else {
+                        div = userInputOne / userInputTwo
+                        Toast.makeText(this, "The sum is $div", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    Toast.makeText(this, "Please fill all the required fields!", Toast.LENGTH_SHORT).show()
+                }
+                binding.etValueOne.setText("")
+                binding.etValueTwo.setText("")
+            }
+        }
     }
 }
-
